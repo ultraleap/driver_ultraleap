0b3358a14cfa0c16b697c365b3801b32bdee249f
Robert Blenkinsopp <robert.blenkinsopp@ultraleap.com>
2023-12-05 14:57:45 +0000

    Fix grab strength in VrHand

--- src/VrHand.h
+++ src/VrHand.h
@@ -17,7 +17,7 @@ class VrHand {
 
     [[nodiscard]] auto GetBoneTransforms() const -> std::span<const vr::VRBoneTransform_t> { return std::span{bones_transforms_}; }
     [[nodiscard]] auto GetPinchStrength() const -> float { return pinch_strength_; }
-    [[nodiscard]] auto GetGrabStrength() const -> float { return pinch_strength_; }
+    [[nodiscard]] auto GetGrabStrength() const -> float { return grab_strength_; }
     [[nodiscard]] auto GetIndexFingerCurl() const -> float { return index_finger_curl_; }
     [[nodiscard]] auto GetMiddleFingerCurl() const -> float { return middle_finger_curl_; }
     [[nodiscard]] auto GetRingFingerCurl() const -> float { return ring_finger_curl_; }
