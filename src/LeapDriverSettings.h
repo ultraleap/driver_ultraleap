1c75307ec910ec87ffb99d4ec0fb26a3f8d8808f
Robert Blenkinsopp <robert.blenkinsopp@ultraleap.com>
2024-04-04 18:43:04 +0100

    Add missing trailing _ from members

--- src/LeapDriverSettings.h
+++ src/LeapDriverSettings.h
@@ -14,21 +14,21 @@ class LeapDriverSettings {
     [[nodiscard]] auto HmdTrackerOffset() const -> VrVec3 { return hmd_tracker_offset_; }
     [[nodiscard]] auto DesktopTrackerOffset() const -> VrVec3 { return desktop_tracker_offset_; }
     [[nodiscard]] auto EnableElbowTrackers() const -> bool { return enable_elbow_trackers_; }
-    [[nodiscard]] auto ExternalInputOnly() const -> bool { return external_input_only ; }
-    [[nodiscard]] auto ExtendedHandProfile() const -> bool { return extended_hand_profile ; }
+    [[nodiscard]] auto ExternalInputOnly() const -> bool { return external_input_only_ ; }
+    [[nodiscard]] auto ExtendedHandProfile() const -> bool { return extended_hand_profile_ ; }
 
     auto UpdateTrackingMode(const eLeapTrackingMode value) -> void { tracking_mode_ = value; }
     auto UpdateHmdTrackerOffset(const VrVec3& value) -> void { hmd_tracker_offset_ = value; }
     auto UpdateDesktopTrackerOffset(const VrVec3& value) -> void { desktop_tracker_offset_ = value; }
     auto UpdateEnableElbowTrackers(const bool value) -> void { enable_elbow_trackers_ = value; }
-    auto UpdateExternalInputOnly(const bool value) -> void { external_input_only  = value; }
-    auto UpdateExtendedHandProfile(const bool value) -> void { extended_hand_profile  = value; }
+    auto UpdateExternalInputOnly(const bool value) -> void { external_input_only_  = value; }
+    auto UpdateExtendedHandProfile(const bool value) -> void { extended_hand_profile_  = value; }
 
   private:
     std::atomic<eLeapTrackingMode> tracking_mode_;
     std::atomic<VrVec3> hmd_tracker_offset_{};
     std::atomic<VrVec3> desktop_tracker_offset_{};
     std::atomic<bool> enable_elbow_trackers_{};
-    std::atomic<bool> external_input_only{};
-    std::atomic<bool> extended_hand_profile{};
+    std::atomic<bool> external_input_only_{};
+    std::atomic<bool> extended_hand_profile_{};
 };
